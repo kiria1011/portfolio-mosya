a754f2906188c4121d672559955aff6a503d214e
kiria1011 <92292163+kiria1011@users.noreply.github.com>
2022-05-06 11:53:26 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const jsSmoothScroll = document.querySelectorAll('a[href^="#"]');
+
+for (let i = 0; i < jsSmoothScroll.length; i++){
+  jsSmoothScroll[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    let href = jsSmoothScroll[i].getAttribute('href');
+    let target = document.getElementById(href.replace('#', ''));
+    const rect = target.getBoundingClientRect().top;
+    const offset = window.pageYOffset;
+    const position = rect + offset;
+    window.scrollTo({
+      top: position,
+      behavior: 'smooth',
+    });
+  });
+}
